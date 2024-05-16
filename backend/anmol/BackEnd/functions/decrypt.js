74436b77ff39b681151406cefbe89a8b18e54b66
Anmol Jain UnitresTech <164000233+AjUnitres@users.noreply.github.com>
2024-05-16 12:36:27 +0530

    Delete backend/anmol/BackEnd directory

--- backend/anmol/BackEnd/functions/decrypt.js
+++
@@ -1,7 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-global.decrypt = async function decrypt(data, key) {
-    const decryptedBytes = CryptoJS.AES.decrypt(data, key);
-    const decryptedData = decryptedBytes.toString(CryptoJS.enc.Utf8);
-    return decryptedData;
-};
