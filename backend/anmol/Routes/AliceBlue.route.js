2e771dfb3da758304894332a33e294a7e9ac2f2d
Anmol Jain UnitresTech <anmol.jain@unitrestech.com>
2024-05-16 15:42:17 +0530

    check2

--- backend/anmol/Routes/AliceBlue.route.js
+++ backend/anmol/Routes/AliceBlue.route.js
@@ -34,9 +34,9 @@ router.post("/", async (req, res) => {
             // const dematAcc = userdata[0].DematAcc ? (userdata[0].DematAcc) : [];
             // const clientExists = dematAcc.some(acc => acc.clientID === clientID);
             
-            if (clientExists) {
-                return res.status(400).json({ error: "Client ID already linked to another account" });
-            }
+            // if (clientExists) {
+            //     return res.status(400).json({ error: "Client ID already linked to another account" });
+            // }
 
             const parentAccValue = dematAcc.length === 0 ? true : false;
             dematAcc.push({ 
