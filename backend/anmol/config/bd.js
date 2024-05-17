7aa3b60e53a7808169a5d0005b6a04759b8be847
Anmol Jain UnitresTech <anmol.jain@unitrestech.com>
2024-05-17 11:25:15 +0530

    done

--- backend/anmol/config/bd.js
+++ backend/anmol/config/bd.js
@@ -17,16 +17,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Anmol@594381',
-  database: process.env.DB_NAME || 'unitradeDatabase',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
-  // host: '127.0.0.1',
-  // user: 'root',
-  // password: 'Anmol@594381',
-  // database: 'unitradeDatabase',
-  // port: 3306,
 });
 
 module.exports = connection;
