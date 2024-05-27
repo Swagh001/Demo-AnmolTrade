5f1f7fe14bbc5a282b85639ad569cc934ad3dba4
shailesh wagh <shaileshwagh300@gmail.com>
2024-05-27 11:24:37 +0530

    FIle added with some changes

--- backend/anmol/index.js
+++ backend/anmol/index.js
@@ -8,6 +8,8 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 app.use(cors());
+const cache = {};
+const CACHE_TTL = 60 * 1000; //
 
 const { User } = require("./model/User");
 const jwtMiddleware = require("./middleware/jwtMiddleware");
@@ -33,6 +35,13 @@ app.get('/', (req, res) => {
 
   app.get('/api', async (req, res) => {
     console.log("Received request at /api endpoint");
+
+    const { symbol } = req.query;
+    const cacheKey = `option-chain-${symbol}`;
+    
+    if (cache[cacheKey] && (Date.now() - cache[cacheKey].timestamp < CACHE_TTL)) {
+        return res.json(cache[cacheKey].data);
+    }
     try {
       const response = await axios.get('https://www.nseindia.com/api/option-chain-indices', {
         params: req.query,
